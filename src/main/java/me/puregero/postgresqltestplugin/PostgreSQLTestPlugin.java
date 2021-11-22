79e7bba594bdb492232459e040fecc19a96f1aef
PureGero <puregero@gmail.com>
2021-11-22 16:10:35 +1000

    Successfully add postgresql library

--- src/main/java/me/puregero/postgresqltestplugin/PostgreSQLTestPlugin.java
+++ src/main/java/me/puregero/postgresqltestplugin/PostgreSQLTestPlugin.java
@@ -2,12 +2,6 @@ package me.puregero.postgresqltestplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,30 +10,11 @@ public class PostgreSQLTestPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Install PostgreSQL - https://github.com/PureGero/PostgreSQLTestPlugin
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
-            // Class not found, let's add it to the classpath
-            File postgresqlJarFile = new File("postgresql.jar");
-            if (!postgresqlJarFile.isFile()) {
-                // The jar file doesn't exist, let's download it
-                getLogger().info("Downloading postgresql.jar...");
-                String url = "https://jdbc.postgresql.org/download/postgresql-42.3.1.jar";
-                try {
-                    Files.copy(new URL(url).openStream(), postgresqlJarFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-            try {
-                // Load the postgresql jar
-                Class.forName("org.postgresql.Driver", true, new URLClassLoader(new URL[] {postgresqlJarFile.toURL()}, getClassLoader()));
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
+            throw new RuntimeException(e);
         }
-        // Finished installing PostgreSQL
 
         try {
             Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "postgres", "123");
@@ -47,4 +22,5 @@ public class PostgreSQLTestPlugin extends JavaPlugin {
             throw new RuntimeException(e);
         }
     }
+
 }
