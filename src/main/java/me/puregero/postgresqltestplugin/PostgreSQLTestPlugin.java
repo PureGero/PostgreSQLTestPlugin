3bbbe58e1576dc87e71e05c950883cdb941de1c2
PureGero <puregero@gmail.com>
2021-11-29 13:48:05 +1000

    Add notification model

--- src/main/java/me/puregero/postgresqltestplugin/PostgreSQLTestPlugin.java
+++ src/main/java/me/puregero/postgresqltestplugin/PostgreSQLTestPlugin.java
@@ -1,23 +1,37 @@
 package me.puregero.postgresqltestplugin;
 
+import com.impossibl.postgres.api.jdbc.PGConnection;
+import com.impossibl.postgres.api.jdbc.PGNotificationListener;
+import com.impossibl.postgres.jdbc.PGDataSource;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PostgreSQLTestPlugin extends JavaPlugin {
+
+    private PGConnection connection;
 
     @Override
     public void onEnable() {
         try {
-            // Load the driver
-            Class.forName("org.postgresql.Driver");
-            // Connect to your database
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/testdb",
-                    "postgres",
-                    "password");
+            PGDataSource ds = new PGDataSource();
+            ds.setServerName("localhost");
+            ds.setDatabaseName("test");
+            ds.setUser("postgres");
+            ds.setPassword("password");
+
+            connection = (PGConnection) ds.getConnection();
+
+            PGNotificationListener listener = new PGNotificationListener() {
+                public void notification(int processId, String channelName, String payload) {
+                    System.out.println("notification = " + payload);
+                }
+            };
+
+            Statement statement = connection.createStatement();
+            statement.execute("LISTEN test");
+            statement.close();
+            connection.addNotificationListener(listener);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
